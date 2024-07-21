a7aa99bfe1bb083ee53ea60de865e306d8fe3a0e
Kishore Jayanthi <kishore.jayanthi9@gmail.com>
2024-07-20 21:15:37 -0600

    Initial changes for crib finder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AuthenticateComponent } from './authenticate/authenticate.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path : 'about' , component : AboutComponent},
+  { path : 'contact' , component : ContactComponent},
+  { path : 'authenticate' , component : AuthenticateComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AuthenticateComponent } from './authenticate/authenticate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AboutComponent,
+    ContactComponent,
+    AuthenticateComponent
   ],
   imports: [
     BrowserModule,
